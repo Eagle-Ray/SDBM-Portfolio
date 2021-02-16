d938859a14571efbd6194b801a0829b2a12017a2
Stacy Brown <sbrown1031@gmail.com>
2021-02-16 04:57:49 -0600

    more formatting

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,14 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+
+
+
+
+
+
+
+
+// tried to do a slideshow
+//   document.addEventListener('DOMContentLoaded', function() {
+    // var elems = document.querySelectorAll('.carousel');
+    // var instances = M.Carousel.init(elems, options);
+//   });
